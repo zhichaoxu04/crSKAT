2a88b93c6810c55f80b37bd9e0efea458cabadfb
Zhichao Xu <xu@Xus-MacBook-Pro.attlocal.net>
2023-10-03 00:02:01 -0500

    example data desc

--- R/data.R
+++ R/data.R
@@ -4,8 +4,8 @@
 #'
 #' @format A list of 2 with 1000 observations
 #' \describe{
-#'   \item{tempTime}{The time-to-event observed time}
-#'   \item{tempType}{The type of causes to the outcome: primary outcome or competing event}
+#'   \item{tempTime}{The time-to-event observed time.}
+#'   \item{tempType}{The type of causes to the outcome: primary outcome (1) or competing event (2).}
 #' }
-#' @source <https://onlinelibrary.wiley.com/doi/10.1111/biom.12109>
+#' @references <https://onlinelibrary.wiley.com/doi/10.1111/biom.12109>
 "exampleData"
